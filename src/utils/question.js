c6ea0b52fa00771b21b05c51112d6e99380ae9db
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-02-09 00:18:26 +0200

    Await question fix

--- src/utils/question.js
+++ src/utils/question.js
@@ -4,7 +4,7 @@ import { promiseQuestion } from './promiseQuestion';
 
 export const question = async question => {
   const rl = readline.createInterface({ input, output });
-  const answer = promiseQuestion(rl, question);
+  const answer = await promiseQuestion(rl, question);
   if (answer.match(/(\d+\s)/) && answer.split(' ').length <= 3) {
     return answer.split(' ').map(i => +i);
   } else {
